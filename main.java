55b3947cbc5b7d8b5485d27bed71085d4dc9c8cb
Viola Mircheva <133656496+ViliMircheva@users.noreply.github.com>
2026-01-14 11:04:53 +0200

    Create main

---
+++ main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(5 + 3);
+        System.out.println(10 * 2);
+        System.out.println(20 - 7);
+        System.out.println(100 / 4);
+    }
+}
